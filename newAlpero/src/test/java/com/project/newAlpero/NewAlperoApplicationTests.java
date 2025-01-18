e7e75197e4c0e064e99ac72a998fbbb776df7b85
tak <lht1711@gmail.com>
2025-01-18 15:44:17 +0900

    Initial commit

---
+++ newAlpero/src/test/java/com/project/newAlpero/NewAlperoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.newAlpero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewAlperoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
